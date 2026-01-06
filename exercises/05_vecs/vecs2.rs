7cf3a97842bc5d9a6726fab4848d4b472f3df09c
satyamdevs <satyam.2428it1663@kiet.edu>
2026-01-06 20:35:51 +0530

    excercise-5,6 done

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -2,12 +2,12 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
     let mut output = Vec::new();
 
     for element in input {
-        // TODO: Multiply each element in the `input` slice by 2 and push it to
-        // the `output` vector.
+        output.push(element * 2);
     }
 
     output
 }
+
 
 fn vec_map_example(input: &[i32]) -> Vec<i32> {
     // An example of collecting a vector after mapping.
@@ -17,17 +17,12 @@ fn vec_map_example(input: &[i32]) -> Vec<i32> {
 }
 
 fn vec_map(input: &[i32]) -> Vec<i32> {
-    // TODO: Here, we also want to multiply each element in the `input` slice
-    // by 2, but with iterator mapping instead of manually pushing into an empty
-    // vector.
-    // See the example in the function `vec_map_example` above.
     input
         .iter()
-        .map(|element| {
-            // ???
-        })
+        .map(|element| element * 2)
         .collect()
 }
+
 
 fn main() {
     // You can optionally experiment here.
--- exercises/06_move_semantics/move_semantics4.rs
+++ exercises/06_move_semantics/move_semantics4.rs
@@ -7,12 +7,17 @@ mod tests {
     // TODO: Fix the compiler errors only by reordering the lines in the test.
     // Don't add, change or remove any line.
     #[test]
-    fn move_semantics4() {
-        let mut x = Vec::new();
-        let y = &mut x;
-        let z = &mut x;
-        y.push(42);
-        z.push(13);
-        assert_eq!(x, [42, 13]);
-    }
+ 
+fn move_semantics4() {
+    let mut x = Vec::new();
+
+    let y = &mut x;
+    y.push(42);
+
+    let z = &mut x;
+    z.push(13);
+
+    assert_eq!(x, [42, 13]);
+}
+
 }
